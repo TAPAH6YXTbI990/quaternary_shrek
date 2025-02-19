84f9eb661a0025b2980a58c710b3466bbf22f691
TAPAH6YXTbI <79250048+TAPAH6YXTbI990@users.noreply.github.com>
2025-02-19 08:16:52 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -11,11 +11,11 @@ int main()
 
     std::string image_path = "C:/Users/User/.vscode/projects/OpenShrek.jpg"; 
    
-    Mat img = imread(image_path, IMREAD_COLOR);
+    Mat img = imread(image_path);
 
     if (img.empty())
     {
-        std::cerr << "Ошибка при загрузке изображения!" << std::endl;
+        std::cerr << "ГЋГёГЁГЎГЄГ  ГЇГ°ГЁ Г§Г ГЈГ°ГіГ§ГЄГҐ ГЁГ§Г®ГЎГ°Г Г¦ГҐГ­ГЁГї!" << std::endl;
         return -1; 
     }
 
@@ -24,23 +24,22 @@ int main()
     int width = img.cols / 2;
     int height = img.rows / 2;
 
-    Mat mask1 = result(Rect(0, 0, width, height));  //левый верхний
-    Mat mask2 = result(Rect(0, height, width, height));  // левый нижний
-    Mat mask3 = result(Rect(width, 0, width, height)); // правый верхний
-    Mat mask4 = result(Rect(width, height, width, height)); //правый нижний
-
-    bitwise_not(mask3,mask3);   
+    Mat mask1 = result(Rect(0, 0, width, height)); 
+    Mat mask2 = result(Rect(0, height, width, height));  
+    Mat mask3 = result(Rect(width, 0, width, height)); 
+    Mat mask4 = result(Rect(width, height, width, height)); 
 
     Mat gray;
     cvtColor(mask2, gray, COLOR_BGR2GRAY); 
     cvtColor(gray, gray, COLOR_GRAY2BGR);
     gray.copyTo(mask2);
-
+    
+    bitwise_not(mask3,mask3);   
+    
     mask4.setTo(Scalar(100,5,255));
 
-    // Показываем изображения
-    imshow("Исходное изображение", img);
-    imshow("Результат", result);
+    imshow("Г€Г±ГµГ®Г¤Г­Г®ГҐ ГЁГ§Г®ГЎГ°Г Г¦ГҐГ­ГЁГҐ", img);
+    imshow("ГђГҐГ§ГіГ«ГјГІГ ГІ", result);
 
     int k = waitKey(0); 
 
